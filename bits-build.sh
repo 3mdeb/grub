9f805db42d8767ae34218fb8ab1c197bce115a20
Kyösti Mälkki <kyosti.malkki@3mdeb.com>
2018-07-20 11:42:10 +0300

    Add call to configure script
    We don't have toplevel Makefile to put this into.
    
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@3mdeb.com>

---
+++ bits-build.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+unexport HOSTCC CC LD OBJCOPY STRIP
+
+cd build
+
+../configure CC="$(HOSTCC)" LD="$(LD)" \
+	TARGET_CC="$(CC)" TARGET_OBJCOPY="$(OBJCOPY)" TARGET_STRIP="$(STRIP)" \
+	CFLAGS=-O2 TARGET_CFLAGS=-Os \
+	--target=i386 --with-platform=coreboot --disable-werror \
+	--disable-nls --disable-efiemu --disable-grub-emu-sdl \
+	--disable-grub-mkfont --disable-grub-mount --disable-device-mapper \
+	--disable-libzfs --enable-boot-time
+
+make
