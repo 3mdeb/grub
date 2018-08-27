7e211c2e0f26745aa8848208a6e3feb5589d7fdb
Piotr Król <piotr.krol@3mdeb.com>
2018-08-27 12:38:45 +0200

    bits-build.sh: remove all flags for debugging purposes
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- bits-build.sh
+++ bits-build.sh
@@ -4,9 +4,7 @@ unset HOSTCC CC LD OBJCOPY STRIP
 
 cd build
 
-../configure CC="$(HOSTCC)" LD="$(LD)" \
-	TARGET_CC="$(CC)" TARGET_OBJCOPY="$(OBJCOPY)" TARGET_STRIP="$(STRIP)" \
-	CFLAGS=-O2 TARGET_CFLAGS=-Os \
+../configure \
 	--target=i386 --with-platform=coreboot --disable-werror \
 	--disable-nls --disable-efiemu --disable-grub-emu-sdl \
 	--disable-grub-mkfont --disable-grub-mount --disable-device-mapper \
