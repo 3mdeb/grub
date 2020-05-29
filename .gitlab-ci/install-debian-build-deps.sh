fb9d0c1a5cb856c7e9b6c8177fe1df86f360c2d3
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2020-05-29 12:51:21 +0200

    gitlab-ci: add build_debian_pkg job
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

---
+++ .gitlab-ci/install-debian-build-deps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y install \
+  build-essential \
+  dh-make \
+  flex \
+  bison \
+  help2man \
+  texinfo \
+  xfonts-unifont \
+  libfreetype6-dev \
+  libdevmapper-dev \
+  libsdl1.2-dev \
+  xorriso \
+  qemu-system \
+  cpio \
+  parted \
+  libfuse-dev \
+  ttf-dejavu-core \
+  liblzma-dev \
+  dosfstools \
+  mtools \
+  wamerican \
+  pkg-config \
+  bash-completion \
+  libefiboot-dev \
+  libefivar-dev \
+  patchutils \
+  gcc-9-multilib \
+  git
